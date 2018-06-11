90c2e82c6ccd4b0b858247254501b681acaa7d04
Dima Rusyy <dima.ru.com@gmail.com>
2018-06-11 19:58:56 +0300

    - changed output directory

--- cmake/flags.cmake
+++ cmake/flags.cmake
@@ -52,3 +52,15 @@ macro(print_compile_flags)
 endmacro()
 
 set(CMAKE_VERBOSE_MAKEFILE TRUE)
+
+###############################################################################
+# project directory
+###############################################################################
+include_directories(${CMAKE_CURRENT_SOURCE_DIR})
+
+###############################################################################
+# output directories
+###############################################################################
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/distro/lib)
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/distro/lib)
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/distro/bin)
