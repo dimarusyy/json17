2c29e6836fa9f114d921f68f7ae4becf153351c2
Dima Rusyy <dima.ru.com@gmail.com>
2018-06-11 18:17:36 +0300

    - fixed cmake support

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,3 +1,16 @@
+################################################## 
+# using vcpkg ?
+################################################## 
+if(NOT DEFINED CMAKE_TOOLCHAIN_FILE)
+	if (EXISTS "$ENV{CMAKE_TOOLCHAIN_FILE}")
+		set(CMAKE_TOOLCHAIN_FILE "$ENV{CMAKE_TOOLCHAIN_FILE}")
+		message("-- Using vcpkg cmake integration : CMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}")
+		include(${CMAKE_TOOLCHAIN_FILE})
+	else()
+		message("-- Skipping vcpkg integration : CMAKE_TOOLCHAIN_FILE=[$ENV{CMAKE_TOOLCHAIN_FILE}]")
+	endif ()
+endif ()
+
 ###############################################################################
 # Boost
 ###############################################################################
@@ -9,8 +22,8 @@ else ()
   message("-- Boost was not found; attempting to download it if we haven't already...")
   include(ExternalProject)
   ExternalProject_Add(install-Boost
-    PREFIX ${CMAKE_BINARY_DIR}/dependencies/boost_1_64_0
-    URL https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.bz2
+    PREFIX ${CMAKE_BINARY_DIR}/dependencies/boost_1_67_0
+    URL https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.bz2
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     INSTALL_COMMAND ""
